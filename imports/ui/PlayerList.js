639ff4d31fd69443f00b5968671b98cc9a2219ae
tracyvincent <tracykvincent@gmail.com>
2017-04-19 08:14:41 -0500

    Impliments FlipMove

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FlipMove from 'react-flip-move';
 
 import Player from './Player';
 
@@ -20,7 +21,9 @@ export default class PlayerList extends React.Component {
   render(){
     return (
       <div>
-        {this.renderPlayers()}
+        <FlipMove maintainContainerHeight={true}>
+          {this.renderPlayers()}
+        </FlipMove>
       </div>
     );
   }
